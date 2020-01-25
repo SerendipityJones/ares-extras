a761673a3dcbabc5df88babb6ea52d5db240afb6
Ren <59793669+SerendipityJones@users.noreply.github.com>
2020-01-25 09:07:07 -0800

    Create scenes-unshared.js

---
+++ plugins/unshared/webportal/routes/scenes-unshared.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import ReloadableRoute from 'ares-webportal/mixins/reloadable-route';
+import DefaultRoute from 'ares-webportal/mixins/default-route';
+
+export default Route.extend(DefaultRoute, ReloadableRoute, {
+    gameApi: service(),
+    
+    model: function() {
+        let api = this.gameApi;
+        return api.requestOne('unsharedScenes');
+    }
+});
